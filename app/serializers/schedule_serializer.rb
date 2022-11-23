644b841d06e8fa66db0476ddb5e4c838c8f7db15
champl0o <podilkaroman@gmail.com>
2022-11-23 23:37:49 +0200

    Update schedules serializer

--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -1,5 +1,13 @@
 class ScheduleSerializer
   include JSONAPI::Serializer
 
-  attributes :id, :start_time, :end_time, :day, :created_at, :updated_at, :consultant
+  attributes :id, :start_time, :end_time, :created_at, :updated_at, :consultant
+
+  attribute :day do |object|
+    object.day.capitalize
+  end
+
+  attribute :slots do |object|
+    (object.start_time.to_s(:time).to_i...object.end_time.to_s(:time).to_i).to_a
+  end
 end
